abee2ab0d815cd8e07de0f43b86f7939c1e199aa
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-21 15:45:40 +0100

    Define the palette colors

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  // a ncreew container dyalna dkchi 3lach dart had ster dyal 'blocklist'
+  blocklist: ["container"],
+  theme: {
+    extend: {
+      colors: {
+        // loun ra2issi mor lbyed w lk7el
+        lightGray: "#f3f4f6",
+        // loun nhtajouh fchi haja bhal l hover, focus...
+        mediumGray: "#e5e7eb",
+        // n9edou manhtajouhch ga3, lmohim kayn hh
+        darkGray: "#d1d5db",
+      }
+    },
+  },
+  plugins: [],
+}
