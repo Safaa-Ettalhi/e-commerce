98ae6f8037826468ea2351b423883c5d9d8fb7a2
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-22 14:23:24 +0100

    Create the script of scrolling in the shoes section images

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -14,3 +14,21 @@ ScrollReveal().reveal(".hero-section", { delay: 200 });
 ScrollReveal().reveal(".arrivals-section", { delay: 200 });
 ScrollReveal().reveal(".categories-section", { delay: 200 });
 ScrollReveal().reveal(".shoes-section", { delay: 200 });
+
+// slider images in shoes section
+const slider = document.getElementById("slider");
+let arrows = document.querySelectorAll(".arrows");
+let firstImage = document.querySelectorAll("#slider > img")[0];
+let imageWidth = firstImage.offsetWidth;
+
+function maxScroll() {
+    return slider.scrollWidth - slider.clientWidth;
+}
+
+arrows.forEach((arrow) => {
+    arrow.addEventListener("click", () => {
+        slider.scrollLeft += arrow.id == "left" ? -imageWidth : imageWidth;
+        if(slider.scrollLeft >= (slider.scrollWidth - slider.clientWidth))
+            slider.scrollLeft = 0;
+    })
+})
