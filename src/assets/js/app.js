aa85d638b9887082de185e722d2645b95f9e9e3b
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-22 02:58:50 +0100

    Create an animation to reveal elements while scrolling

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,3 +4,13 @@ const menu = document.getElementById("menu");
 menu_icon.addEventListener("click", () => {
     menu.classList.toggle("active");
 })
+
+ScrollReveal({
+    distnace: '0px',
+    duration: 2500,
+});
+
+ScrollReveal().reveal(".hero-section", { delay: 200 });
+ScrollReveal().reveal(".arrivals-section", { delay: 200 });
+ScrollReveal().reveal(".categories-section", { delay: 200 });
+ScrollReveal().reveal(".shoes-section", { delay: 200 });
